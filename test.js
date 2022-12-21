08f50cf242fc0ed6fa079014c6d892b2e1778507
MagicDev119 <melnykovychadrian@gmail.com>
2022-12-21 22:06:06 +0200

    scraping from indeed

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
-const indeed = require('indeed-scraper');
+var axios = require('axios');
 
-const queryOptions = {
-  host: 'www.indeed.com',
-  query: 'Software',
-  city: 'Seattle, WA',
-  radius: '25',
-  level: 'entry_level',
-  jobType: 'fulltime',
-  maxAge: '7',
-  sort: 'date',
-  limit: 100
+var config = {
+  method: 'get',
+  url: 'https://www.indeed.com/jobs?q=full+time&l=United+States&sc=0bf%3Aexrec%28%29%3B&fromage=1',
+  headers: {
+    'Cookie': 'CTK=1gkr2cjv7irp8800; __cf_bm=_GCDxSYHCC0O.aPLo8B11b.Ca6ktROGhGUKrGUwYwrA-1671650759-0-AYtibmBMhFTdWdPmWSJ1+kbVNdFFzPabpHZnGcpoPhgWZmSexe11uv93lwU+KZzxPqB71dqfe+8/x5PX72Xmw5Y=; _cfuvid=S3dvB2fyLPLS5e39AtsgVDDW1ueW9UgIceExwphXkIE-1671650759695-0-604800000; INDEED_CSRF_TOKEN=ZTBiFfEttWAwQ0bmSXfMleOiv9RMC4zE; JSESSIONID=DE000806ACDDF5823D8CEEC11C754A7B; LV="LA=1671650758:CV=1671650758:TS=1671650758"; PREF="TM=1671650758639:L=United+States"; RQ="q=full+time&l=United+States&ts=1671650758677&sc=0bf%3Aexrec%28%29%3B"; UD="LA=1671650758:CV=1671650758:TS=1671650758:SG=47a413ba027610c3bb07b881f1b715f7"; ctkgen=1; indeed_rcc=""; jaSerpCount=1'
+  }
 };
 
-indeed.query(queryOptions).then(res => {
-  console.log(res); // An array of Job objects
-});
+axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
