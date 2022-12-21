60db8a1da9f8d366150c16e412c879f73f443ea0
MagicDev119 <melnykovychadrian@gmail.com>
2022-12-21 22:01:07 +0200

    scraping from indeed

--- scraping.js
+++ scraping.js
@@ -15,6 +15,11 @@ const scrapingFunc = async (pageStartNumber) => {
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
   ]
 
+  const headerInfo = {
+    'Cookie': 'CTK=1gkr2cjv7irp8800; __cf_bm=_GCDxSYHCC0O.aPLo8B11b.Ca6ktROGhGUKrGUwYwrA-1671650759-0-AYtibmBMhFTdWdPmWSJ1+kbVNdFFzPabpHZnGcpoPhgWZmSexe11uv93lwU+KZzxPqB71dqfe+8/x5PX72Xmw5Y=; _cfuvid=S3dvB2fyLPLS5e39AtsgVDDW1ueW9UgIceExwphXkIE-1671650759695-0-604800000; INDEED_CSRF_TOKEN=ZTBiFfEttWAwQ0bmSXfMleOiv9RMC4zE; JSESSIONID=DE000806ACDDF5823D8CEEC11C754A7B; LV="LA=1671650758:CV=1671650758:TS=1671650758"; PREF="TM=1671650758639:L=United+States"; RQ="q=full+time&l=United+States&ts=1671650758677&sc=0bf%3Aexrec%28%29%3B"; UD="LA=1671650758:CV=1671650758:TS=1671650758:SG=47a413ba027610c3bb07b881f1b715f7"; ctkgen=1; indeed_rcc=""; jaSerpCount=1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+  }
+
   const config = {
     baseSiteUrl: `https://www.indeed.com`,
     startUrl: `https://www.indeed.com/jobs?q=full+time&l=United+States&sc=0bf%3Aexrec%28%29%3B&fromage=1`,
@@ -22,7 +27,7 @@ const scrapingFunc = async (pageStartNumber) => {
     maxRetries: 1,
     cloneFiles: false,
     logPath: './logs/',
-    headers: { 'User-Agent': user_agents_list[2] }
+    headers: headerInfo
   }
 
   const scraper = new Scraper(config)
