6278a6b14c8d601956cc7d2189f61b3330d00e54
MagicDev119 <melnykovychadrian@gmail.com>
2022-12-21 22:59:40 +0200

    scraping from indeed

--- scraping.js
+++ scraping.js
@@ -16,8 +16,12 @@ const scrapingFunc = async (pageStartNumber) => {
   ]
 
   const headerInfo = {
-    'Cookie': 'CTK=1gkr2cjv7irp8800; __cf_bm=_GCDxSYHCC0O.aPLo8B11b.Ca6ktROGhGUKrGUwYwrA-1671650759-0-AYtibmBMhFTdWdPmWSJ1+kbVNdFFzPabpHZnGcpoPhgWZmSexe11uv93lwU+KZzxPqB71dqfe+8/x5PX72Xmw5Y=; _cfuvid=S3dvB2fyLPLS5e39AtsgVDDW1ueW9UgIceExwphXkIE-1671650759695-0-604800000; INDEED_CSRF_TOKEN=ZTBiFfEttWAwQ0bmSXfMleOiv9RMC4zE; JSESSIONID=DE000806ACDDF5823D8CEEC11C754A7B; LV="LA=1671650758:CV=1671650758:TS=1671650758"; PREF="TM=1671650758639:L=United+States"; RQ="q=full+time&l=United+States&ts=1671650758677&sc=0bf%3Aexrec%28%29%3B"; UD="LA=1671650758:CV=1671650758:TS=1671650758:SG=47a413ba027610c3bb07b881f1b715f7"; ctkgen=1; indeed_rcc=""; jaSerpCount=1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    'Cookie': 'CSRF=CcqjWbsRiJ8tCkEZq32tOoPpd4QidtGU; CTK=1gkr2cjv7irp8800; __cf_bm=70AgrTi9T5P3JQVH4YabfFQLATpdyf_xcPN4Iu17okI-1671653530-0-AXdA/f2ykdgV5AfrgphUy3n/UCLzyKx+TQbixdrWIU0J0iMXMGWPJlkcd74tH+JhbQ4PcGenhgk4187yDr0SpXA=; _cfuvid=S3dvB2fyLPLS5e39AtsgVDDW1ueW9UgIceExwphXkIE-1671650759695-0-604800000; INDEED_CSRF_TOKEN=ZTBiFfEttWAwQ0bmSXfMleOiv9RMC4zE; JSESSIONID=DE000806ACDDF5823D8CEEC11C754A7B; LV="LA=1671650758:CV=1671650758:TS=1671650758"; PREF="TM=1671650758639:L=United+States"; RQ="q=full+time&l=United+States&ts=1671650758677&sc=0bf%3Aexrec%28%29%3B"; UD="LA=1671650758:CV=1671650758:TS=1671650758"; indeed_rcc="PREF:LV:RQ:UD"; jaSerpCount=1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0'
   }
 
   const config = {
